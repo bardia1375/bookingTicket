f92aaaa9c5be0196f81b65f0a14c451267a06205
Bardiashams <bardiasham1375@gmail.com>
2023-12-04 16:58:21 +0330

    first

---
+++ my-react-redux-app/src/redux/reducer.ts
@@ -0,0 +1,12 @@
+// src/redux/reducer.ts
+const initialState = {
+    // Define your initial state here
+  };
+  
+  const rootReducer = (state = initialState, action: any) => {
+    // Handle actions and update the state
+    return state;
+  };
+  
+  export default rootReducer;
+  
